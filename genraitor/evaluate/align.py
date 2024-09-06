274afb92209dd10a68e706502a17879f3d0351ef
M. Jensen <matt.jensen@pnnl.gov>
2024-09-06 13:04:50 -0700

    batch size = 1.

--- genraitor/evaluate/align.py
+++ genraitor/evaluate/align.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 from alignscore import AlignScore
 
-from ..conf import env
+from ..conf import env, log
 
 
 def evaluate(tokenizer, model, data, batch_size = 15) -> pd.DataFrame:
@@ -17,13 +17,11 @@ def evaluate(tokenizer, model, data, batch_size = 15) -> pd.DataFrame:
         ckpt_path=str(Path(env.paths.app) / "data/alignscore/AlignScore-base.ckpt"),
         evaluation_mode="nli_sp",
     )
-    chunks = batch_dataframe(data, batch_size)
-
+    log.info(f"eval {len(data)} records")
     results = []
-    for chunk in chunks:
-        claims = chunk["claim"].to_list()
-        contexts = chunk["context"].to_list()
-
+    for _, row in data.iterrows():
+        claims = [row["claim"]]
+        contexts = [row["context"]]
 
         inputs = tokenizer(contexts, return_tensors="pt", padding=True)
         ids = inputs["input_ids"].to(device)
@@ -35,31 +33,15 @@ def evaluate(tokenizer, model, data, batch_size = 15) -> pd.DataFrame:
         scores = scorer.score(contexts=contexts, claims=pred_claims)
         pred_scores = pd.DataFrame(scores, columns=["align_score"])
         pred_scores["dataset"] = "pred"
-        pred_scores["context"] = contexts
-        pred_scores["claim"] = pred_claims
+        # pred_scores["context"] = row["context"]
+        # pred_scores["claim"] = row["claim"]
         results.append(pred_scores)
 
         scores = scorer.score(contexts=contexts, claims=claims)
         eval_scores = pd.DataFrame(scores, columns=["align_score"])
-        eval_scores["context"] = contexts
-        eval_scores["claim"] = claims
+        # eval_scores["context"] = row["context"]
+        # eval_scores["claim"] = row["claim"]
         eval_scores["dataset"] = "eval"
         results.append(eval_scores)
 
-        return pd.concat(results)
-
-def batch_dataframe(df, n):
-    """
-    Splits a dataframe into chunks of size n.
-
-    :param df: The input dataframe
-    :param n: The size of each chunk
-    :return: A list of dataframes, each of size n (last one might be smaller)
-    """
-    # Determine the number of chunks
-    num_chunks = len(df) // n + (1 if len(df) % n != 0 else 0)
-
-    # Split the dataframe into chunks
-    chunks = [df.iloc[i * n:(i + 1) * n] for i in range(num_chunks)]
-
-    return chunks
+    return pd.concat(results)
